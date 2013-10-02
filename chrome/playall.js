e48219b759c57e48c3870e391f82945520760ed0
jimkang <jimkang@gmail.com>
2013-10-02 12:34:23 -0400

    Now working, in a very basic sense: - Added player state change listener to queue the next video in the list when playback stops. - Renamed playAll to start and moved playing code to playNext().

--- chrome/playall.js
+++ chrome/playall.js
@@ -1,20 +1,32 @@
 // This is meant to run in the context of the post page in a <script> tag.
 
 var PlayAll = {
-  URLs: [],
+  videoIds: [],
+  nowPlayingIndex: -1,
   player: null
 };
 
-PlayAll.playAll = function playAll() {
+PlayAll.start = function start() {
   this.player = document.getElementById('playAllPlayer');
   if (!this.player) {
+    console.log('Playall could not get the video player.');
     return;
   }
-  this.URLs = this.collectYouTubeURLs();
+  
+  PlayAll.player.addEventListener('onStateChange', 'onYouTubePlayerStateChange');
 
-  if (this.URLs.length < 1) {
+  this.videoIds = this.collectYouTubeIds();
+
+  if (this.videoIds.length < 1) {
     return;
   }
+
+  this.playNext();
+};
+
+PlayAll.playNext = function playNext() {
+  ++this.nowPlayingIndex;
+  this.player.loadVideoById(this.videoIds[this.nowPlayingIndex]);
 };
 
 PlayAll.createPlayerContainerElement = function createPlayerContainerElement() {
@@ -35,16 +47,48 @@ PlayAll.createPlayerContainerElement = function createPlayerContainerElement() {
   return playerContainer;
 };
 
-PlayAll.collectYouTubeURLs = function collectYouTubeURLs() {
-  var URLs = [];
+PlayAll.collectYouTubeIds = function collectYouTubeIds() {
+  var ids = [];
   var linkEls = document.querySelectorAll('a[href*="youtube.com"]');
   for (var i = 0; i < linkEls.length; ++i) {
     var linkEl = linkEls[i];
-    URLs.push(linkEl.href);
+    var theId = this.getQueryParamFromURL(linkEl.href, 'v');
+    if (theId) {
+      ids.push(theId);
+    }
   }
-  return URLs;
-}
+  return ids;
+};
+
+PlayAll.getQueryParamFromURL = function getQueryParamFromURL(url, param) {
+  var val = null;
+  var linkParts = url.split('?');
+  if (linkParts.length > 1) {
+    var queryString = linkParts[1];
+    var queryParts = queryString.split('&');
+    for (var i = 0; i < queryParts.length; ++i) {
+      var queryPart = queryParts[i];
+      var keyAndValue = queryPart.split('=');
+      if (keyAndValue.length === 2) {
+        if (keyAndValue[0] === param) {
+          val = keyAndValue[1];
+          break;
+        }
+      }
+    }
+  }
+  return val;
+};
 
 function onYouTubePlayerReady(playerId) {
-  PlayAll.playAll();
+  PlayAll.start();
 }
+
+function onYouTubePlayerStateChange(newState) {
+  // YT.PlayerState.ENDED
+  if (0 === newState) {
+    debugger;
+    PlayAll.playNext();
+  }
+};
+
