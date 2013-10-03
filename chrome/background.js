dd5231f6cdbe48f7b68da686d979aa7a84e86a2e
jimkang <jimkang@gmail.com>
2013-10-03 00:51:23 -0400

    Got rid of unnecessary pagecomber.js load in browserAction responder.

--- chrome/background.js
+++ chrome/background.js
@@ -1,20 +1,11 @@
 function doWhenSwfObjectIsLoaded() {
   chrome.tabs.executeScript(null, {
-    file: "pagecomber.js",
-    allFrames: true
-  },
-  doWhenPageComberIsLoaded);
-}
-
-function doWhenPageComberIsLoaded() {
-  chrome.tabs.executeScript(null, {
     file: "playerloader.js",
     allFrames: true
   });
 }
 
 chrome.browserAction.onClicked.addListener(function respondToClick(tab) {
-  console.log('Yo guy.');
   chrome.tabs.executeScript(null, {
     file: "swfobject.js",
     allFrames: true
